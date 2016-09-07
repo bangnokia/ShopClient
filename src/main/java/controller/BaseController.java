58ecb4f17219fdbc12794705e2ea87b3c73d5503
bimbimsweet <trunghieuhp2000@gmail.com>
2016-09-07 16:15:31 +0700

    hieu

--- src/main/java/controller/BaseController.java
+++ src/main/java/controller/BaseController.java
@@ -3,8 +3,36 @@ package controller;
 import java.sql.*;
 
 public class BaseController {
-    public Connection connectDB(){
-        Connection con= null;
-        return con;
-    } 
+
+    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_CONNECTION = "jdbc:mysql://127.0.0.1:3306/project?zeroDateTimeBehavior=convertToNull";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "";
+
+    private static Connection getDBConnection() {
+
+        Connection dbConnection = null;
+
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+            return dbConnection;
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return dbConnection;
+    }
+
+    public static void main(String[] args) {
+        Connection con = getDBConnection();
+
+    }
+
 }
