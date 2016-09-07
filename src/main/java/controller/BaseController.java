03d8292c9fe59a5c1d1d55b3801345f2a5e21bef
bimbimsweet <trunghieuhp2000@gmail.com>
2016-09-07 16:42:18 +0700

    connect data ok

--- src/main/java/controller/BaseController.java
+++ src/main/java/controller/BaseController.java
@@ -1,11 +1,13 @@
 package controller;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BaseController {
 
     private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_CONNECTION = "jdbc:mysql://127.0.0.1:3306/project?zeroDateTimeBehavior=convertToNull";
+    private static final String DB_CONNECTION = "jdbc:mysql://127.0.0.1:3306/project";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "";
 
@@ -32,6 +34,20 @@ public class BaseController {
 
     public static void main(String[] args) {
         Connection con = getDBConnection();
+        if (con != null) {
+            System.out.println("connec ok");
+        }
+
+        Statement stmt;
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from image");
+            while (rs.next()) {
+                System.out.println(rs.getString(2) + "," + rs.getString(3));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
